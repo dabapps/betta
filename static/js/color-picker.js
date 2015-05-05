de43246b3f5c1af8c22e4a27331e44097a98eaa2
JakeSidSmith <jake@dabapps.com>
2015-05-05 13:55:56 +0100

    Basic color picker implementation

--- static/js/color-picker.js
+++ static/js/color-picker.js
@@ -1,19 +1,237 @@
 'use strict';
 
-window.define(['react'], function (React) {
+window.define(['react', 'color'], function (React, color) {
 
-  var ColorPicker = React.createClass({
+  var Values = React.createClass({
     render: function () {
       var self = this;
 
       return React.createElement(
         'div',
         {
-          className: 'color-picker',
-          style: {
-            backgroundColor: self.props.value || self.props.defaultValue
+          className: 'row'
+        },
+        React.createElement(
+          'div',
+          {
+            className: 'col-xs-12'
+          },
+          React.createElement(
+            'code',
+            null,
+            'hsl(' + [self.props.hsl.h, Math.round(self.props.hsl.s * 100) + '%', Math.round(self.props.hsl.l * 100) + '%'].join(', ') + ')'
+          )
+        ),
+        React.createElement(
+          'div',
+          {
+            className: 'col-xs-12'
+          },
+          React.createElement(
+            'code',
+            null,
+            'rgb(' + [self.props.rgb.r, self.props.rgb.g, self.props.rgb.b].join(', ') + ')'
+          )
+        ),
+        React.createElement(
+          'div',
+          {
+            className: 'col-xs-12'
+          },
+          React.createElement(
+            'code',
+            null,
+            self.props.hex
+          )
+        )
+      );
+    }
+  });
+
+  var Palette = React.createClass({
+    addListeners: function () {
+      window.addEventListener('mousemove', this.mouseMove);
+      window.addEventListener('mouseup', this.mouseUp);
+    },
+
+    removeListeners: function () {
+      window.removeEventListener('mousemove', this.mouseMove);
+      window.removeEventListener('mouseup', this.mouseUp);
+    },
+
+    getPoint: function (event) {
+      var box = this.getDOMNode().getBoundingClientRect();
+
+      var xInPalette = Math.min(Math.max(event.clientX - box.left, 0), box.width);
+      var yInPalette = Math.min(Math.max(event.clientY - box.top, 0), box.height);
+
+      var point = {
+        x: Math.min(Math.max((xInPalette / (box.width / 100)) / 100, 0), 100),
+        y: Math.min(Math.max((yInPalette / (box.height / 100)) / 100, 0), 100)
+      };
+
+      return point;
+    },
+
+    mouseDown: function (event) {
+      event.preventDefault();
+      var point = this.getPoint(event);
+
+      this.props.setPoint(point);
+
+      this.addListeners();
+    },
+
+    mouseMove: function (event) {
+      var point = this.getPoint(event);
+
+      this.props.setPoint(point);
+    },
+
+    mouseUp: function () {
+      this.removeListeners();
+    },
+
+    componentWillUnmount: function () {
+      this.removeListeners();
+    },
+
+    render: function () {
+      var self = this;
+
+      return React.createElement(
+        'div',
+        {
+          className: 'background',
+          onMouseDown: self.mouseDown
+        },
+        React.createElement(
+          'div',
+          {
+            className: 'gradient grey'
           }
-        }
+        ),
+        React.createElement(
+          'div',
+          {
+            className: 'gradient white'
+          }
+        ),
+        React.createElement(
+          'div',
+          {
+            className: 'gradient black'
+          }
+        ),
+        React.createElement(
+          'div',
+          {
+            className: 'point',
+            style: {
+              top: self.props.point.y * 100 + '%',
+              left: self.props.point.x * 100 + '%',
+              border: '1px solid ' + (self.props.point.y >= 50 ? '#ccc' : '#333')
+            }
+          }
+        )
+      );
+    }
+  });
+
+  var ColorPicker = React.createClass({
+    setPoint: function (point) {
+      var hsl = this.state.hsl;
+      hsl.s = Math.round(point.x * 100) / 100;
+      hsl.l = Math.round((1 - point.y) * 100) / 100;
+
+      var rgb = color.HSLToRGB(hsl.h, hsl.s, hsl.l);
+      var hex = color.RGBToHex(rgb.r, rgb.g, rgb.b);
+
+      this.setState({
+        point: point,
+        hsl: hsl,
+        rgb: rgb,
+        hex: hex
+      });
+    },
+
+    togglePicker: function () {
+      this.setState({
+        active: !this.state.active
+      });
+    },
+
+    getInitialState: function () {
+      return {
+        active: false,
+        point: {
+          x: 0,
+          y: 0
+        },
+        hsl: {
+          h: 0,
+          s: 0,
+          l: 1
+        },
+        rgb: {
+          r: 255,
+          g: 255,
+          b: 255
+        },
+        hex: '#ffffff'
+      };
+    },
+
+    render: function () {
+      var self = this;
+      var picker;
+
+      if (this.state.active) {
+        picker = React.createElement(
+          'div',
+          {
+            className: 'picker'
+          },
+          React.createElement(
+            'div',
+            {
+              className: 'palette'
+            },
+            React.createElement(
+              Palette,
+              {
+                point: self.state.point,
+                setPoint: self.setPoint
+              }
+            )
+          ),
+          React.createElement(
+            Values,
+            {
+              hsl: self.state.hsl,
+              rgb: self.state.rgb,
+              hex: self.state.hex
+            }
+          )
+        );
+      }
+
+      return React.createElement(
+        'div',
+        {
+          className: 'color-picker'
+        },
+        React.createElement(
+          'div',
+          {
+            className: 'swatch',
+            style: {
+              backgroundColor: self.props.value || self.props.defaultValue
+            },
+            onClick: self.togglePicker
+          }
+        ),
+        picker
       );
     }
   });
---
+++ static/js/color.js
@@ -0,0 +1,108 @@
+'use strict';
+
+window.define([], function () {
+
+  var color = {
+    HSLToRGB: function (h, s, l) {
+      var r, g, b;
+
+      if (s === 0) {
+        r = g = b = l;
+      } else {
+        var hueToRGB = function hue2rgb(p, q, t){
+          if (t < 0) {
+            t += 1;
+          }
+          if (t > 1) {
+            t -= 1;
+          }
+          if (t < 1/6) {
+            return p + (q - p) * 6 * t;
+          }
+          if (t < 1/2) {
+            return q;
+          }
+          if (t < 2/3) {
+            return p + (q - p) * (2/3 - t) * 6;
+          }
+          return p;
+        };
+
+        var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        var p = 2 * l - q;
+        r = hueToRGB(p, q, h + 1/3);
+        g = hueToRGB(p, q, h);
+        b = hueToRGB(p, q, h - 1/3);
+      }
+
+      return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+      };
+    },
+
+    RGBToHSL: function (r, g, b) {
+      r = r / 255;
+      g  = g / 255;
+      b = b / 255;
+
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+      var h, s, l = (max + min) / 2;
+
+      if (max === min) {
+        h = s = 0;
+      } else {
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+
+        switch (max) {
+          case r:
+            h = (g - b) / d + (g < b ? 6 : 0);
+            break;
+          case g:
+            h = (b - r) / d + 2;
+            break;
+          case b:
+            h = (r - g) / d + 4;
+            break;
+        }
+
+        h /= 6;
+      }
+
+      return {
+        h: Math.round(h * 255),
+        s: Math.round(s * 100),
+        l: Math.round(l * 100)
+      };
+    },
+
+    RGBToHex: function (r, g, b) {
+      var colorToHex = function (c) {
+        var hex = c.toString(16);
+        return hex.length === 1 ? '0'.concat(hex) : hex;
+      };
+
+      return ['#', colorToHex(r), colorToHex(g), colorToHex(b)].join('');
+    },
+
+    HexToRGB: function (hex) {
+      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return [r, r, g, g, b, b].join('');
+      });
+
+      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+      } : undefined;
+    }
+  };
+
+  return color;
+
+});
