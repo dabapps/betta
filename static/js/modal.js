57c43afd7a17cb7872a56226325f1849aaab73e3
JakeSidSmith <jake@dabapps.com>
2015-05-19 11:45:46 +0100

    Create modal

---
+++ static/js/modal.js
@@ -0,0 +1,107 @@
+'use strict';
+
+window.define(['react', 'modal-store'], function (React, ModalStore) {
+
+  var Modal = React.createClass({
+    modalChanged: function () {
+      this.setState({
+        open: ModalStore.isOpen(),
+        title: ModalStore.getTitle(),
+        body: ModalStore.getBody(),
+        footer: ModalStore.getFooter()
+      });
+    },
+
+    close: function () {
+      ModalStore.action('close');
+    },
+
+    componentDidMount: function () {
+      this.setState({
+        fadeClass: 'in',
+        display: 'block'
+      });
+    },
+
+    componentWillUnmount: function () {
+      ModalStore.unbind('open', this.modalChanged);
+      ModalStore.unbind('close', this.modalChanged);
+    },
+
+    componentWillMount: function () {
+      ModalStore.bind('open', this.modalChanged);
+      ModalStore.bind('close', this.modalChanged);
+    },
+
+    getInitialState: function () {
+      return {
+        open: ModalStore.isOpen(),
+        title: ModalStore.getTitle(),
+        body: ModalStore.getBody(),
+        footer: ModalStore.getFooter()
+      };
+    },
+
+    render: function () {
+      if (this.state.open) {
+        return React.createElement(
+          'div',
+          {
+            className: 'modal fade ' + this.state.fadeClass,
+            style: {
+              display: this.state.display
+            }
+          },
+          React.createElement(
+            'div',
+            {
+              className: 'modal-overlay',
+              onClick: this.close
+            }
+          ),
+          React.createElement(
+            'div',
+            {
+              className: 'modal-dialog'
+            },
+            React.createElement(
+              'div',
+              {
+                className: 'modal-content'
+              },
+              React.createElement(
+                'div',
+                {
+                  className: 'modal-header'
+                },
+                React.createElement(
+                  'h4',
+                  null,
+                  this.state.title
+                )
+              ),
+              React.createElement(
+                'div',
+                {
+                  className: 'modal-body'
+                },
+                this.state.body
+              ),
+              React.createElement(
+                'div',
+                {
+                  className: 'modal-footer'
+                },
+                this.state.footer
+              )
+            )
+          )
+        );
+      }
+      return false;
+    }
+  });
+
+  return Modal;
+
+});
