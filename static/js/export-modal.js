724cfc16f3244c98a2a24afab336a6c42b7f5048
JakeSidSmith <jake@dabapps.com>
2015-05-21 01:43:37 +0100

    Display settings with checkbox component

--- static/js/export-modal.js
+++ static/js/export-modal.js
@@ -1,23 +1,66 @@
 'use strict';
 
-window.define(['react', 'modal-template', 'modal-store', 'variable-store'], function (React, ModalTemplate, ModalStore, VariableStore) {
+window.define(['react', 'modal-template', 'modal-store', 'variable-store', 'checkbox', 'export-settings-store'], function (React, ModalTemplate, ModalStore, VariableStore, Checkbox, ExportSettingsStore) {
 
   var ExportModal = React.createClass({
+    updateSetting: function (settingsIndex, event) {
+      var value = event.target.parentNode.getElementsByTagName('input')[0].checked;
+      ExportSettingsStore.action('updateSetting', settingsIndex, value);
+    },
+
+    getSettings: function () {
+      var settings = ExportSettingsStore.getSettings();
+
+      this.setState({
+        settings: settings,
+        packedVariables: this.getPackedVariables(settings)
+      });
+    },
+
     close: function () {
       ModalStore.action('close');
     },
 
+    getPackedVariables: function (settings) {
+      return VariableStore.getPackedVariables.apply(
+        null,
+        settings.map(function (setting) {
+          return setting.value;
+        })
+      );
+    },
+
+    componentWillUnmount: function() {
+      ExportSettingsStore.unbind('updateSetting', this.getSettings);
+    },
+
     componentWillMount: function () {
-      this.setState({
-        packedVariables: VariableStore.getPackedVariables(true, true, false, true)
-      });
+      ExportSettingsStore.bind('updateSetting', this.getSettings);
     },
 
     getInitialState: function () {
-      return {};
+      var settings = ExportSettingsStore.getSettings();
+
+      return {
+        settings: settings,
+        packedVariables: this.getPackedVariables(settings)
+      };
     },
 
     render: function () {
+      var self = this;
+
+      var settings = this.state.settings.map(function (setting, settingsIndex) {
+        return React.createElement(
+          Checkbox,
+          {
+            checked: setting.value,
+            label: setting.name,
+            onClick: self.updateSetting.bind(this, settingsIndex)
+          }
+        );
+      });
+
       return React.createElement(
         ModalTemplate,
         {
@@ -25,6 +68,19 @@ window.define(['react', 'modal-template', 'modal-store', 'variable-store'], func
           body: React.createElement(
             'div',
             null,
+            React.createElement(
+              'div',
+              {
+                className: 'row'
+              },
+              React.createElement(
+                'div',
+                {
+                  className: 'col-xs-12'
+                },
+                settings
+              )
+            ),
             React.createElement(
               'pre',
               {
@@ -35,9 +91,9 @@ window.define(['react', 'modal-template', 'modal-store', 'variable-store'], func
             React.createElement(
               'textarea',
               {
-                className: 'variable-textarea'
-              },
-              this.state.packedVariables
+                className: 'variable-textarea',
+                value: this.state.packedVariables
+              }
             )
           ),
           footer: React.createElement(
