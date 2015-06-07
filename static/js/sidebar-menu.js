40474fc651fa84c6703f99a0c57b756dbb7d91f0
JakeSidSmith <jake@dabapps.com>
2015-06-07 12:54:26 +0100

    Remove huge comment

--- static/js/sidebar-menu.js
+++ static/js/sidebar-menu.js
@@ -1,22 +1,5 @@
 'use strict';
 
-/*
-
-<div class="dropdown">
-  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
-    Dropdown
-    <span class="caret"></span>
-  </button>
-  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-  </ul>
-</div>
-
-*/
-
 window.define(['react', 'modal-store', 'export-modal', 'search-store'], function (React, ModalStore, ExportModal, SearchStore) {
 
   var SidebarMenu = React.createClass({
