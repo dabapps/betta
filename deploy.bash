8b854009d009531c1e6ae70c2938679d851d602b
Emmanouil Konstantinidis <manos@dabapps.com>
2015-06-23 03:15:40 +0300

    Minor Changes

--- deploy.bash
+++ deploy.bash
@@ -1,28 +1,28 @@
 #!/usr/bin/env bash
+
 BRANCH=gh-pages
 TARGET_REPO=dabapps/betta.git
 DIST_FOLDER=dist
 
-echo -e "Testing travis-encrypt"
-echo -e "$VARNAME"
-
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    echo -e "Starting deployment on Github Pages\n"
+    echo -e "Starting deployment to Github Pages\n"
+
     if [ "$TRAVIS" == "true" ]; then
         git config --global user.email "manos@iamemmanouil.com"
         git config --global user.name "ekonstantinidis"
     fi
 
-    #using token clone gh-pages branch
+    # Using token clone gh-pages branch
     git clone --quiet --branch=$BRANCH https://${GH_TOKEN}@github.com/$TARGET_REPO built_website > /dev/null
 
-    #go into directory and copy data we're interested in to that directory
+    # Go into directory and copy data we're interested in to that directory
     cd built_website
     rsync -rv --exclude=.git  ../$DIST_FOLDER/* .
 
-    #add, commit and push files
+    # Add, commit and push files
     git add -f .
-    git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to Github Pages"
+    git commit -m "Deployment - Travis build $TRAVIS_BUILD_NUMBER"
     git push -fq origin $BRANCH > /dev/null
-    echo -e "Deploy completed\n"
+
+    echo -e "Deployment completed\n"
 fi
