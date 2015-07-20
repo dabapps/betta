b601eccc52666d5fb402b0ddc0c81953f5ce5412
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-07-20 19:02:43 +0100

    Fix tests

--- static/js/components/navigation.js
+++ static/js/components/navigation.js
@@ -19,7 +19,7 @@ var Navigation = React.createClass({
       <nav className='navbar navbar-inverse navbar-fixed-top'>
         <div className='container-fluid'>
           <div className='navbar-header'>
-            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+            <button type='button' className='navbar-toggle collapsed'>
               <span className='sr-only'>Toggle navigation</span>
               <span className='icon-bar'></span>
               <span className='icon-bar'></span>
