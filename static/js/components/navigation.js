5423bb0e6745e406c7eed3e5a8f6d00e3fca7310
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-07-20 18:56:41 +0100

    Navbar Styling

--- static/js/components/navigation.js
+++ static/js/components/navigation.js
@@ -5,25 +5,26 @@ var React = require('react');
 var Navigation = React.createClass({
   render: function () {
     return (
-      <nav className="navbar navbar-inverse navbar-fixed-top">
-        <div className="container">
-          <div className="navbar-header">
-            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span className="sr-only">Toggle navigation</span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
+      <nav className='navbar navbar-inverse navbar-fixed-top'>
+        <div className='container-fluid'>
+          <div className='navbar-header'>
+            <button type='button' className='navbar-toggle collapsed' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar'>
+              <span className='sr-only'>Toggle navigation</span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
+              <span className='icon-bar'></span>
             </button>
-            <a className="navbar-brand" href="#">Betta</a>
+            <a className='navbar-brand' href='/'>
+              <img className='img-responsive app-logo' src='static/img/logo-icon.png' />
+            </a>
           </div>
-          <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav">
-              <li className="active"><a href="/">Back</a></li>
-              <li><a href="/">Import</a></li>
-              <li><a href="/">Save</a></li>
+          <div className='navbar-collapse collapse'>
+            <ul className='nav navbar-nav'>
+              <li><a href='/'>Import</a></li>
+              <li><a href='/'>Save</a></li>
             </ul>
-            <ul className="nav navbar-nav navbar-right">
-              <li><a target="_blank" href="https://github.com/dabapps/betta">Fork us on GitHub</a></li>
+            <ul className='nav navbar-nav navbar-right'>
+              <li><a href='/'>Right Side</a></li>
             </ul>
           </div>
         </div>
