347ed5b377a494eb42d7db1e8be2fbbbb3e6cec8
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-07-20 18:30:01 +0100

    Init Navbar content

--- static/js/components/navigation.js
+++ static/js/components/navigation.js
@@ -5,9 +5,29 @@ var React = require('react');
 var Navigation = React.createClass({
   render: function () {
     return (
-      <div className='nav'>
-        NAVIGATION
-      </div>
+      <nav className="navbar navbar-inverse navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">Betta</a>
+          </div>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="/">Back</a></li>
+              <li><a href="/">Import</a></li>
+              <li><a href="/">Save</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li><a target="_blank" href="https://github.com/dabapps/betta">Fork us on GitHub</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 });
